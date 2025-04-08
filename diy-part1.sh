84c5b6bc4a1720c5d7839abcfb24ae5cae3027fd
tongduychuong <66476208+tongduychuong@users.noreply.github.com>
2025-04-08 08:48:17 +0700

    change package to tongduychuong package

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,12 +9,16 @@
 #=============================================================
 
 # fw876/helloworld
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.defaultault
+#sed -i 's/^#\(.*packages\)/\1/' feeds.conf.default
+
+sed -i '1d' feeds.conf.default
 
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 #sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
 
 #sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+
+sed -i '1i src-git packages https://github.com/tongduychuong/packages.git;openwrt-23.05' feeds.conf.default
 
 #sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
